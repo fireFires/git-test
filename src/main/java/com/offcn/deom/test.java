9d313057dd1955fc6269e0e574c08f89d525fc30
yuxi <2605321997@qq.com>
2022-08-30 00:02:36 +0800

    第一次

---
+++ src/main/java/com/offcn/deom/test.java
@@ -0,0 +1,11 @@
+package com.offcn.deom;
+
+/**
+ * @Author yuxi
+ * @Date 2022 08 30 00 01
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+    }
+}
